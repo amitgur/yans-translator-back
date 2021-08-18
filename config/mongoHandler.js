787e6043d0b92d5c0a9572057ff38a427560b71b
Amit Office <amit@menagenet.co.il>
2021-08-18 12:12:00 +0300

    mongo uri

--- config/mongoHandler.js
+++ config/mongoHandler.js
@@ -10,7 +10,16 @@ exports.connectDBS = function (mongodbURI) {
 
   console.log("\r\nConnected to Databases:");
   dbs.forEach((db) => {
-    cons[db] = mongoHandler.createConnection(`mongodb://localhost:27017/${db}`);
+
+    let uri;
+
+    if (process.env.MONGO_CONNECT_TYPE === 'local') {
+      uri = `mongodb://localhost:27017/${db}`;
+    } else {
+      uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@localhost:27017/${db}`
+    }
+
+    cons[db] = mongoHandler.createConnection(uri);
     console.log("   \x1b[36;1m✓ %s\x1b[0m", db);
   });
   console.log();
