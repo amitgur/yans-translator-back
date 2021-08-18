3ea9c3b7ca8189abbffeb089991b7ac45b43b99c
Amit Office <amit@menagenet.co.il>
2021-08-18 14:16:11 +0300

    dev:

--- bash/restart.sh
+++ bash/restart.sh
@@ -4,10 +4,10 @@
 
 echo 'starting YANS on server with forever'
     echo "Stopping node thread"
-    forever stop yans_back.js
+    forever stop yans_translator_back.js
 
     echo "Restarting bandpad-idm-back"
     # user
     rm ./log/forever.log
 
-    forever start -l ./log/forever.log yans_back.js
+    forever start -l /home/bandpad/bandpad/yans-translator_back/log/forever.log yans_translator_back.js
